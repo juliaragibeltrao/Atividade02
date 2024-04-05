b1e3d9bb7923cb0b3df29ca4aa275b4bfaef1428
juliaragibeltrao <julia.beltrao@alunos.unis.edu.br>
2024-04-04 23:13:31 -0300

    Exercicio1

---
+++ Exercicio1.php
@@ -0,0 +1,100 @@
+<?php
+$num = 1; 
+
+echo "Tabuada do $numero:\n";
+for ($n = 1; $n <= 10; $n++) {
+    echo "$num x $n = " . ($num * $n) . "\n";
+}
+?>
+
+<?php
+
+$num = 2; 
+
+echo "Tabuada do $numero:\n";
+for ($n = 1; $n <= 10; $n++) {
+    echo "$num x $n = " . ($num * $n) . "\n";
+}
+?>
+
+<?php
+
+$num = 3; 
+
+echo "Tabuada do $numero:\n";
+for ($n = 1; $n <= 10; $n++) {
+    echo "$num x $n = " . ($num * $n) . "\n";
+}
+?>
+
+<?php
+
+$num = 4; 
+
+echo "Tabuada do $numero:\n";
+for ($n = 1; $n <= 10; $n++) {
+    echo "$num x $n = " . ($num * $n) . "\n";
+}
+?>
+
+<?php
+
+$num = 5; 
+
+echo "Tabuada do $numero:\n";
+for ($n = 1; $n <= 10; $n++) {
+    echo "$num x $n = " . ($num * $n) . "\n";
+}
+?>
+
+
+<?php
+
+$num = 6; 
+
+echo "Tabuada do $numero:\n";
+for ($n = 1; $n <= 10; $n++) {
+    echo "$num x $n = " . ($num * $n) . "\n";
+}
+?>
+
+<?php
+
+$num = 7; 
+
+echo "Tabuada do $numero:\n";
+for ($n = 1; $n <= 10; $n++) {
+    echo "$num x $n = " . ($num * $n) . "\n";
+}
+?>
+
+<?php
+
+$num = 8; 
+
+echo "Tabuada do $numero:\n";
+for ($n = 1; $n <= 10; $n++) {
+    echo "$num x $n = " . ($num * $n) . "\n";
+}
+?>
+
+<?php
+
+$num = 9; 
+
+echo "Tabuada do $numero:\n";
+for ($n = 1; $n <= 10; $n++) {
+    echo "$num x $n = " . ($num * $n) . "\n";
+}
+?>
+
+<?php
+
+$num = 10; 
+
+echo "Tabuada do $numero:\n";
+for ($n = 1; $n <= 10; $n++) {
+    echo "$num x $n = " . ($num * $n) . "\n";
+}
+?>
+
